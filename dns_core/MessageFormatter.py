d6c0c1b41ec1171263d87bd49d91d49df39ca082
lerotsy <rotsyrazafy@gmail.com>
2023-12-26 14:03:47 -0500

    Add question parsing

--- dns_core/MessageFormatter.py
+++ dns_core/MessageFormatter.py
@@ -1,12 +1,18 @@
 import struct
 from .constants import TYPE_A, CLASS_IN
 
+
 class MessageFormatter:
     @staticmethod
-    def parse_question(question_section: bytes = b''):
+    def parse_question(buffer: bytes = b''):
         from .QuestionDetails import QuestionDetails
-        # hardcoded for now
-        return QuestionDetails(qname='codecrafters.io', qtype=TYPE_A, qclass=CLASS_IN )
+        if len(buffer) <= 12:
+            raise ValueError("The buffer must be bigger than the header")
+        qname = extract_qname(buffer[12:])
+        size = len(qname) + 4
+        # not used for now because we hardcode qtype and qclass
+        question_section = buffer[12:size]
+        return QuestionDetails(qname=qname, qtype=TYPE_A, qclass=CLASS_IN)
 
     @staticmethod
     def parse_header(header: bytes):
@@ -14,7 +20,8 @@ class MessageFormatter:
         if len(header) != 12:
             raise ValueError("header must be of length 12")
 
-        id, flags, qdcount, ancount, nscount, arcount = struct.unpack('>HHHHHH', header)
+        id, flags, qdcount, ancount, nscount, arcount = struct.unpack(
+            '>HHHHHH', header)
         qr = 1
         opcode = (flags >> 11) & 0b1111
         rd = (flags >> 8) & 0b1
@@ -35,10 +42,12 @@ class MessageFormatter:
     @staticmethod
     def process(buffer: bytes) -> bytes:
         from .AnswerDetails import AnswerDetails
-        header_section = buffer[:12] # header always the first 12 bytes
+        header_section = buffer[:12]  # header always the first 12 bytes
         header = MessageFormatter.parse_header(header_section)
-        question = MessageFormatter.parse_question()
-        answer = AnswerDetails(rname='codecrafters.io', rtype=TYPE_A, rttl=60, rclass=CLASS_IN, rdata=b'\x08\x08\x08\x08')
+        question = MessageFormatter.parse_question(buffer)
+        # answer = AnswerDetails(rname='codecrafters.io', rtype=TYPE_A, rttl=60, rclass=CLASS_IN, rdata=b'\x08\x08\x08\x08')
+        answer = AnswerDetails(rname=question.qname, rtype=TYPE_A,
+                               rttl=60, rclass=CLASS_IN, rdata=b'\x08\x08\x08\x08')
         # breakpoint()
         return (
             header.to_bytes()
@@ -47,10 +56,15 @@ class MessageFormatter:
         )
 
 
-    
 def encode_name(url: str) -> bytes:
     labels = url.split('.')
     encoded_labels = [bytes([len(l)]) + l.encode('utf-8') for l in labels]
     # Add the null terminator at the end
     encoded_labels.append(b'\x00')
     return b''.join(encoded_labels)
+
+
+def extract_qname(seq: bytes) -> bytes:
+    # Find the first occurrence of \x00 and split the byte string
+    # to extract the qname section, then append \x00
+    return seq.split(b'\x00')[0] + b'\x00'
--- dns_core/QuestionDetails.py
+++ dns_core/QuestionDetails.py
@@ -1,15 +1,18 @@
 from dataclasses import dataclass
 import struct
+from typing import Union
 
 @dataclass
 class QuestionDetails:
-    qname: str  # Question Name
+    qname: Union[str, bytes]  # Question Name
     qtype: int  # Question Type (16 bits)
     qclass: int # Question Class (16 bits)
 
     def __post_init__(self):
         from .MessageFormatter import encode_name
-        self.qname = encode_name(self.qname)
+        # if we pass a string like 'codecracters.io' we need encode it
+        if isinstance(self.qname, str):
+            self.qname = encode_name(self.qname)
 
     def to_bytes(self) -> bytes:
         return self.qname + struct.pack(">HH", self.qtype, self.qclass)
